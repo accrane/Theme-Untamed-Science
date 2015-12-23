75bd6448a8d9edf85a73f5eea29831e2afce7b45
Austin Crane <austincrane@gmail.com>
2015-12-23 16:41:10 -0500

    move template part to folder

---
+++ includes/blog-post.php
@@ -0,0 +1,48 @@
+<div class="blog-square item">
+
+	<div class="blog-square-readmore">
+		<a href="<?php the_permalink(); ?>">Read More</a>
+	</div>
+
+
+	<div class="blog-square-category">
+		<?php 
+			$custom_tax = get_the_term_list( $post->ID, 'blogcats', '<li>', '', '</li>');
+			echo $custom_tax;
+		?>
+	</div><!-- blog square category -->
+
+    <div class="blog-featured-image">
+    	<a href="<?php the_permalink(); ?>">
+			<?php
+            //  Display the featured image. Must be inside a loop.
+            if ( has_post_thumbnail() ) {
+                the_post_thumbnail('full');
+            }
+            // If you do not have a Featured Image, show a thumbnail stored in the themes images folder.
+            else {
+                echo '<img src="' . get_bloginfo( 'template_url' ) . '/images/default-featured.png" width="515px; height="412px" />';
+                 }
+            ?>
+            </a>
+    </div><!-- blog featured image  -->
+
+
+	<div class="clear"></div>
+
+	<div class="blog-square-below">
+		<h2>
+			<a href="<?php the_permalink(); ?>">
+			<?php 
+				if(get_field('alternate_title')) {  
+					the_field('alternate_title');  
+				} else {  
+					the_title();  
+				} 
+			?>
+			</a>
+		</h2>
+	</div><!-- blog-square-below -->
+  
+  
+</div><!-- blog square  -->
