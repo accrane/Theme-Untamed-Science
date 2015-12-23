68be780737d2101e18f249426d1d0f53af4f8f6c
Austin Crane <austincrane@gmail.com>
2015-12-23 17:00:54 -0500

    moved to get template part blog-post

--- taxonomy-blogcats.php
+++ taxonomy-blogcats.php
@@ -34,49 +34,11 @@ $taxAlternateTitle = get_field('tax_alternate_title', $taxonomy . '_' . $term_id
 
 <?php
 if(have_posts()) : ?>
-<?php while(have_posts()) : the_post(); ?>
-<div class="blog-square item">
+<?php while(have_posts()) : the_post(); 
 
-<div class="blog-square-readmore"><a href="<?php the_permalink(); ?>">Read More</a></div>
-<!--<div class="filed-away">Filed as:</div>-->
-<div class="blog-square-category">
-<?php $custom_tax = get_the_term_list( $post->ID, 'blogcats', '<li>', '', '</li>') ?>  
-<?php echo $custom_tax ?>
-</div><!-- blog square category -->
+    get_template_part('includes/blog-post');
 
-        <div class="blog-featured-image">
-        <a href="<?php the_permalink(); ?>">
-				<?php
-                //  Display the featured image. Must be inside a loop.
-                if ( has_post_thumbnail() ) {
-                    the_post_thumbnail('full');
-                }
-                // If you do not have a Featured Image, show a thumbnail stored in the themes images folder.
-                else {
-                    echo '<img src="' . get_bloginfo( 'template_url' ) . '/images/default-featured.png" width="515px; height="412px" />';
-                     }
-                ?>
-                </a>
-        </div><!-- blog featured image  -->
-<div class="clear"></div>
-<div class="blog-square-below">
-
-<h2>
-<a href="<?php the_permalink(); ?>">
-<?php if(get_field('alternate_title')) { ?>
-<?php the_field('alternate_title'); ?>
-<?php } else { ?>
-<?php the_title(); ?>
-<?php } ?>
-</a>
-</h2>
-
-<?php  //echo get_excerpt(300); ?> 
-</div><!-- blog-square-below -->
-  
-  
-</div><!-- blog square  -->
-<?php endwhile; ?>
+ endwhile; ?>
     <div class="untamed-pagi">
     <?php untamed_pagi(); ?>
     </div>
