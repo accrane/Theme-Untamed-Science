bfec8dc167f5ab741146a1c9bde8210009d8a802
Austin Crane <austincrane@gmail.com>
2015-12-24 10:52:49 -0500

    fixed closing list items and conditional for parent page

--- sidebar-biology.php
+++ sidebar-biology.php
@@ -1,3 +1,29 @@
+<?php 
+
+if( is_page('world-biology') ) { ?>
+
+	<div class="sideitem grey">
+		<div class="taxonomy-item">
+
+			<h2>The Biology Portal</h2>
+
+		<?php $biology_pages = array(
+					'depth'        => 1,
+					'title_li'     => '',
+					'post_type'    => 'biology',
+				    'post_status'  => 'publish' 
+				); 
+
+		 wp_list_pages($biology_pages); 
+
+		 ?>
+		
+
+	</div><!-- taxonomy item -->
+	</div><!-- sideitem -->
+
+<?php } else { ?>
+
 <div class="sideitem grey">
 <div class="taxonomy-item">
 <?php if(count(get_post_ancestors($post->ID)) == 1 ) { ?>
@@ -20,28 +46,31 @@
 	); 
 	 wp_list_pages($biology_pages); 
  ?>
-</ul>
+
 <?php } else { // if count is == 1 ?>
 
 	<h2><?php echo get_page(array_pop(get_post_ancestors($post->ID)))->post_title; ?></h2>
-<?php 
-	$biology_pages = array(
-		'depth'        => 1,
-		'child_of'     => $post->ID,
-		'exclude'      => '',
-		'include'      => '',
-		'title_li'     => '',
-		'echo'         => 1,
-		'authors'      => '',
-		'sort_column'  => 'menu_order, post_title',
-		'walker'       => '',
-		'post_type'    => 'biology',
-	    'post_status'  => 'publish' 
-	); 
-	 wp_list_pages($biology_pages); 
- ?>
-</ul>
+	
+	<?php 
+		$biology_pages = array(
+			'depth'        => 1,
+			'child_of'     => $post->ID,
+			'exclude'      => '',
+			'include'      => '',
+			'title_li'     => '',
+			'echo'         => 1,
+			'authors'      => '',
+			'sort_column'  => 'menu_order, post_title',
+			'walker'       => '',
+			'post_type'    => 'biology',
+		    'post_status'  => 'publish' 
+		); 
+		 wp_list_pages($biology_pages); 
+	 ?>
+
 
 <?php } /// end if 3 levels deep. ?>
 </div><!-- taxonomy item -->
 </div><!-- sideitem -->
+
+<?php  } ?>
--- sidebar-filmmaking.php
+++ sidebar-filmmaking.php
@@ -1,3 +1,34 @@
+<?php if(is_page('how-to-filmmaking')) { ?>
+
+
+<div class="sideitem grey">
+<div class="taxonomy-item">
+	<h2>
+		<?php 
+				if(get_field('alternate_title')) {  
+					the_field('alternate_title');  
+				} else {  
+					the_title();  
+				} 
+			?>
+	</h2>
+<?php 
+	$filmmakingPages = array(
+		'depth'        => 1,
+		'title_li'     => '',
+		'post_type'    => 'filmmaking',
+	    'post_status'  => 'publish' 
+	); 
+	wp_list_pages($filmmakingPages); 
+?>
+
+
+
+</div><!-- taxonomy item -->
+</div><!-- sideitem -->
+
+<?php } else { ?>
+
 <div class="sideitem grey">
 <div class="taxonomy-item">
 
@@ -20,7 +51,7 @@
 	); 
 	wp_list_pages($filmmakingPages); 
 ?>
-</ul>
+
 <?php } else { // if post count == 1 ?>
 
 	<h2><?php echo get_page(array_pop(get_post_ancestors($post->ID)))->post_title; ?></h2>
@@ -39,8 +70,10 @@
 	    'post_status'  => 'publish' 
 	); 
 	wp_list_pages($filmmakingPages); ?>
-</ul>
+
 <?php } // End Filmmaking ?>
 
 </div><!-- taxonomy item -->
 </div><!-- sideitem -->
+
+<?php } /// else ?>
