7eda270ff7f01cc69446b579979ee629719438d0
Austin Crane <austincrane@gmail.com>
2015-12-30 17:03:24 -0500

    make footer list of other sites into wp menu

--- footer.php
+++ footer.php
@@ -16,71 +16,11 @@
 
 
 <?php if ( !is_home() ) : ?>
-        
-       
-        <div id="footer-slider-container">
-        <h3>New From Untamed Science</h3>
-        	<div id="front-page-rotation">
-             <div id="flexslider">
-                <div class="flexslider">
-                <ul id="mycarousel" class="jcarousel-skin-tango">
-            
-            
-           <?php
-		   
-		   /* Custom Query to pull all posts with the Front Page Category Custom Taxonomy
-		      from 4 different Custom Post Types that are listed in the Array Below */
-		   
-           $args = array(
-		   
-            'post_type' => array('blog','biodiversity', 'filmmaking', 'biology'), //You list of Custom Post Types
-            'posts_per_page' => '100', // # of posts to show
-			'tax_query' => array(  //Custom Taxonomy "front_page"
-				array(       // array within an array
-					'taxonomy' => 'front_page', // Name when registering CT
-					'field' => 'slug',
-					'terms' => 'front-page' // slug created by WP when you make your entry
-				)
-			)
-                
-            );
-            $query = new WP_Query( $args );  // Query all of your arguments from above
-           ?>
-           <?php if (have_posts()) : while( $query->have_posts() ) : $query->the_post(); // the loop ?>
-           
-           <li class="hideitemonload">
-           		<?php $post_type = get_post_type( get_the_ID() ); // Get the post type so you can style your div according to the post type ?>
-           
-                      <div class="homepage-thumb <?php echo $post_type."-home" // post type turned into a class ?>">
-                      <a href="<?php the_permalink(); ?>" >
-                        <?php if ( has_post_thumbnail() ) {  ?>
-                               <?php the_post_thumbnail( 'homepage_thumb' ); ?>
-                        <?php } else { ?><img src="<?php bloginfo('template_url'); ?>/images/default-homepage-thumb.png" /><?php } ?>
-                             <div class="homepage-thumb-title-slider">
-                             <?php if(get_field('alternate_title')) { ?>
-<?php the_field('alternate_title'); ?>
-<?php } else { ?>
-<?php the_title(); ?>
-<?php } ?>
-                             </div><!-- homepage-thumb title slider -->
-                         </a>
-                          </div><!-- homepage thumb --> 
-              
-            </li>
-            <?php  endwhile; endif; wp_reset_postdata();  // close loop ?>
-             <!-- Rewind and Reset -->
-				<?php  //wp_reset_query(); // Reset Query  ?> 
-                <?php  //rewind_posts(); ?>
-            </div><!-- #frobt page rotation -->
-            </ul><!-- /slides -->
-			</div><!-- /flexslider -->
-            </div><!-- /flexslider -->
-            
-            
-            </div><!-- /footer slider container -->
-            
-            
-         <?php endif; ?>
+  <div id="footer-slider-container">
+    <h3>New From Untamed Science</h3>
+    <?php get_template_part('includes/posts-slider'); ?>
+  </div><!-- /footer slider container -->
+<?php endif; ?>
     
     
     
@@ -91,18 +31,12 @@
 
     
     <div class="footer-left">
-    <h3>Company Pages</h3>
-    <ul>
-    <?php wp_nav_menu( array( 'theme_location' => 'footermenu' ) ); ?>
-    <!--<li><a href="<?php bloginfo('url'); ?>/about">About the site</a></li>
-    <li><a href="<?php bloginfo('url'); ?>/crew">Crew information</a></li>
-    <li><a href="<?php bloginfo('url'); ?>/media">Media and Awards</a></li>
-    <li><a href="<?php bloginfo('url'); ?>/privacy">Privacy Policy</a></li>
-    <li><a href="<?php bloginfo('url'); ?>/sitemap">Sitemap</a></li>
-    <li><a href="<?php bloginfo('url'); ?>/wp-login.php">Login</a></li>-->
-    </ul>
-    <p class="copyright">Copyright &copy; <?php echo date('Y'); ?></p>
-    <p class="copyright">All rights reserved.</p>
+      <h3>Company Pages</h3>
+      <ul>
+        <?php wp_nav_menu( array( 'theme_location' => 'footermenu' ) ); ?>
+      </ul>
+      <p class="copyright">Copyright &copy; <?php echo date('Y'); ?></p>
+      <p class="copyright">All rights reserved.</p>
     </div><!-- #footer -->
     
     
@@ -125,9 +59,9 @@
     </div><!-- #footer -->
     
     <div class="footer-mid-right">
-<!-- Begin MailChimp Signup Form -->
-<h3>Join our mailing list</h3>
-<?php get_template_part( '/includes/newsletter' ); ?>
+    <!-- Begin MailChimp Signup Form -->
+      <h3>Join our mailing list</h3>
+      <?php get_template_part( '/includes/newsletter' ); ?>
     </div><!-- #footer -->
     
     <div class="footer-right">
@@ -142,14 +76,10 @@
         </div>
         
     <div class="clear"></div>
+
     <h3>Our other sites:</h3>
         <ul>
-            
-            <!--<li><a href="http://untamedscience.com/year-of-chemistry/">Untamed Chemistry</a></li>-->
-            <li><a href="http://untamedscience.com/how-to-filmmaking">Science Filmaking</a></li>
-            <li><a href="http://thewildclassroom.com">The Wild Classroom</a></li>
-            <li><a href="http://explorebiodiversity.com">Explore Biodiversity</a></li>
-            <li><a href="http://blackbearinfo.com">Black Bear Info</a></li>
+            <?php wp_nav_menu( array( 'theme_location' => 'othersites' ) ); ?>
         </ul>
     </div><!-- #footer -->
     
@@ -166,22 +96,6 @@
     // Can also be used with $(document).ready()
 
    $(window).load(function() {
- /* $('.flexslider').flexslider({
-    animation: "slidereffect",
-        animationLoop: true,
-        easing: "linear",
-        useCSS: false,
-        randomize: false,
-        pauseOnHover: true,
-        slideshowSpeed: 12,
-        animationSpeed: 8000,
-        controlNav: false,
-        directionNav: true,
-        itemWidth: 220,
-        itemMargin: 5,
-		
-  });
-  */
   
   // set up and load the front page carousel
   jQuery('#mycarousel').jcarousel({
@@ -198,7 +112,7 @@
 
   
 });
-  </script>
+</script>
   <!-- Place this tag after the last +1 button tag. -->
 <script type="text/javascript">
   (function() {
